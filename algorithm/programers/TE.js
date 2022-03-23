395090b28f35bedd0abbaddc19c312d8e2f0e0b4
sungmin <s4223425@naver.com>
2022-03-23 09:43:19 +0900

    day 07

--- algorithm/programers/TE.js
+++ algorithm/programers/TE.js
@@ -1,10 +1,15 @@
-function solution(n) {
-    let count = 0;
-    for (let j = 1; j <= num; j++) {
-        if ((left + i) % j === 0) {
-            arr.push(j);
-            if ((left + i) / j !== j) {
-                arr.push((left + i) / j);
-            }
+function solution(s) {
+    //짝수 번째 대문자
+    //공백 기준 나누기
+    let arr = s.split(' ')
+    arr[0].toUpperCase()
+    arr.forEach((v, i) => {
+        for (let j = 0; j < v.length; j++) {
+            if (v[i] % 2 === 0) {
+                arr[i].toUpperCase()
+            } arr[i].toLowerCase()
         }
-    }}
+    })
+    console.log(arr)
+}
+solution("try hello world")
---
+++ class/07-02-docker-with-express/index.js
@@ -0,0 +1,119 @@
+//express
+import express from 'express';
+//modules
+import { checkValidationPhone, sendTokenToSMS, getToken } from './phone.js'
+//express swagger
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+import { options } from './swagger/config.js'
+//.env라이브러리
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { checkValidationEmail, getWelcomeTemplate, sendTemplateToEmail } from './email.js'
+
+
+
+const openapiSpecification = swaggerJsdoc(options);
+
+const app = express()
+const port = 4500
+
+
+//restful-api방식
+//json parse
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+//게시글 목록보기
+// 명세를 잘 작성해야 협업이 좋음.
+// 프론트 개발은 명세 부분을 보고함.
+
+app.get('/boards', (req, res) => {
+    //1. 데이터를 조회하는 로직=>DB에 접속해서 데이터 꺼내오기
+    const result = [
+        {
+            number: 1,
+            writer: '철수',
+            title: "제목1",
+            contents: "내용1"
+        },
+        {
+            number: 2,
+            writer: '영희',
+            title: "제목2",
+            contents: "내용2"
+        },
+        {
+            number: 3,
+            writer: '훈이쓰',
+            title: "제목3",
+            contents: "내용3"
+        },
+    ];
+    // 2. 꺼내온 결과 응답 주기
+    res.send(result);
+})
+
+//게시글 등록하기
+app.post('/boards', (req, res) => {
+    //1. 데이터를 조회하는 로직=>DB에 접속해서 데이터 저장하기
+    // 프론트엔드로부터 데이터 받아오기
+    // 콘솔로 찍기
+    //2. 저장결과 알려주기!
+    res.send('등록 성공!');
+
+    //req.body받은 정보출력
+    console.log(req.body);
+})
+
+//token만들기
+app.post('/tokens/phone', (req, res) => {
+    const myphone = req.body.phoneNumber;
+    // 1. 핸드폰 자릿수 확인.
+    const isValid = checkValidationPhone(myphone);
+    if (isValid === true) {
+        // 2.핸드폰 토큰 6자리 만들기
+        const mytoken = getToken()
+
+        // 3. 핸드폰 번호에 토큰 전송하기
+        sendTokenToSMS(myphone, mytoken)
+        res.send('인증완료!!');
+    }
+})
+
+
+
+app.post('/users', (req, res) => {
+    const myuser = req.body.user;
+
+    const isValid = checkValidationEmail(myuser.email)
+    if (isValid) {
+        const template = getWelcomeTemplate(myuser)
+
+        sendTemplateToEmail(template, myuser.email);
+        res.send("가입완료!")
+    }
+})
+
+
+
+
+// //특정게시글 아이디만 보기
+// app.get('/boards/:id', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// //게시글 수정하기
+// app.put('/boards/:id',(req,res) => {
+//     res.send('');
+// })
+
+// //게시글 삭제하기
+// app.delete('/boards/:id',(req,res) => {
+//     res.send('');
+// })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
