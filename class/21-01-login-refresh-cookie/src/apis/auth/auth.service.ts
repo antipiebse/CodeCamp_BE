f15b515ad57b71b8dc615eee5b404d0fa7303a13
antipiebse <antipiebse@gmail.com>
2022-04-12 16:06:34 +0900

    day21

---
+++ class/21-01-login-refresh-cookie/src/apis/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from '@nestjs/jwt'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService:JwtService
+  ){}
+   getAccessToken({user}){
+     return this.jwtService.sign(
+       { email: user.email, sub: user.id},
+       { secret: "myAccessKey", expiresIn: '1h'}
+     )
+
+   }
+   //토큰의 유효기간을 만들어야 보안이 좋음 
+}
